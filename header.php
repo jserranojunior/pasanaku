b37457a58088a47e7121e85376b8ba092b33afcd
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2017-11-01 16:22:51 -0200

    Bootstrap 3 - Tela de Saldo

--- header.php
+++ header.php
@@ -2,18 +2,50 @@
 <html lang="pt-br">
     <head>
         <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- CSS -->
-        <link href="css/bootstrap.css" rel="stylesheet">        
-        <link href="css/bootstrap-reboot.css" rel="stylesheet">
-        <link href="css/bootstrap-grid.css" rel="stylesheet">
-
+        <link href="css/bootstrap.css" rel="stylesheet">     
+        <link href="https://bootswatch.com/united/bootstrap.css" rel="stylesheet">   
         <!-- JS -->
-        <script src="js/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" crossorigin="anonymous"></script>
         <script src="js/bootstrap.js" crossorigin="anonymous"></script>
-        <script src="js/bootstrap.bundle.js" crossorigin="anonymous"></script>
 
     </head>
     <body>
    
+    <div class="container-fluid">
+   <div class="row">
+        <div class="col">  
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">PASANAKU</a>
+                    </div>
+                
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                        <li><a href="#">SALDO</a></li>   
+                        <li><a href="#">EXTRATO</a></li>    
+                        <li><a href="#">DEPOSITO</a></li>   
+                        <li><a href="#">RESGATE</a></li>   
+                        <li><a href="#">SALDO</a></li>   
+                        <li><a href="#">GRUPO</a></li>             
+                        </ul>                    
+                        <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">SAIR</a></li>                        
+                        </ul>
+                        </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </div>  
+    </div>
--- index.php
+++ index.php
@@ -2,25 +2,47 @@
 include("header.php");
 ?>
 
-<div class="container-fluid">
-   <div class="row">
-        <div class="col-12">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">PASANAKU</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Features</a>
-                <a class="nav-item nav-link" href="#">Pricing</a>
-                <a class="nav-item nav-link disabled" href="#">Disabled</a>
-                </div>
-            </div>
-            </nav>
-        </div>        
-    </div>
+
+
+<div class="row">
+    <div class="col-sm-12">
+        <h3 class="text-center">SALDO</h3>
+        
+      
+            
+                <table class="table table-condensed table-hover">
+                    <tr>
+                        <td>
+                            Valor
+                        </td>
+                        <td class="text-right">
+                            R$10.000,00
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Poupança
+                        </td>
+                        <td class="text-right">
+                            R$10.000,00
+                        </td>
+                    </tr>
+                                                   
+                    <tr class="warning">
+                        <td class="text-bold">
+                            Total
+                        </td>
+                        <td class="text-right">
+                            R$10.000,00
+                        </td>
+                    </tr>
+                </table>
+
+
+
+                
+        
+    </div>    
 </div>
 
 

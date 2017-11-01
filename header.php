c52fdc713a366769de0e2f2a0df5eaf4c8692fe3
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2017-11-01 15:30:37 -0200

    bootstrap 4

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">        
+        <link href="css/bootstrap-reboot.css" rel="stylesheet">
+        <link href="css/bootstrap-grid.css" rel="stylesheet">
+
+        <!-- JS -->
+        <script src="js/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.js" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.bundle.js" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+   

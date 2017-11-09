bf1da1ebc66e08a06308faf2b979fde0fe79c082
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2017-11-09 13:55:39 -0200

    Mudanda a rota para inicio

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home', function () {
+    return view('inicio');
+});
 
 
 Route::get('/inicio', function () {

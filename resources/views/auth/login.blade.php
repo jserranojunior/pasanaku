290178362d57f4a7d663392b6e57869821ac78c8
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2017-11-28 14:52:35 -0200

    RM msg erro login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,11 +17,7 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                               
                             </div>
                         </div>
 
@@ -31,11 +27,7 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
     
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                               
                             </div>
                         </div>
 

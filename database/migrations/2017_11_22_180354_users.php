71ff1969c0e5892f4f9d1385f6e98e78fb7875d4
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2017-11-28 11:32:55 -0200

    Removendo mudanças image

--- database/migrations/2017_11_22_180354_users.php
+++ database/migrations/2017_11_22_180354_users.php
@@ -14,7 +14,7 @@ class users extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {            
-            $table->string('image');           
+            $table->string('image')->nullable();           
         });
     }
 

00cabd0622d270e9497cc26148fd07874303066a
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2017-12-05 10:57:48 -0200

    clientes

--- app/Models/Admin/Clientes.php
+++ app/Models/Admin/Clientes.php
@@ -18,17 +18,11 @@ class Clientes extends Model
                 $sel->celular = null;
             }
 
-
-
-
-            
             if(($sel->data_nascimento == "0000-00-00") or ($sel->data_nascimento == "")){
                 $sel->data_nascimento = null;
             }else{
                 $sel->data_nascimento = date('d/m/Y', strtotime($sel->data_nascimento));    
             }
-
-            
 
             if($sel->cpf == 0){
                 $sel->cpf = null;

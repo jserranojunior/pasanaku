467ba18fbae6e716403f01b10599e8db5bdf85e6
Jorge <jorgeserranojunior@hotmail.com>
2018-01-15 20:59:19 -0200

    smart

--- resources/views/creativeinicio.blade.php
+++ resources/views/creativeinicio.blade.php
@@ -89,7 +89,7 @@
                   <hr class="primary">
               </div>
           </div>
-      </div>
+      </div> 
       <div class="container">
           <div class="row">
               <div class="col-lg-4 col-md-4 col-xs-12 text-center">
